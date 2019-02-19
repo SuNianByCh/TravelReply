7617a8ca887c72f21ad64def4b83f121108284fa
sunian <530065946@qq.com>
2019-02-19 15:35:19 +0800

    test

--- reply/src/main/java/com/example/reply/ReplyAdapter.kt
+++ reply/src/main/java/com/example/reply/ReplyAdapter.kt
@@ -2,17 +2,34 @@ package com.example.reply
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 
 class ReplyAdapter : BaseQuickAdapter<ReplyBean, ReplyViewHold>(R.layout.item_reply) {
 
+    var mZanParentListener: IOnClickListener<ReplyBean>? = null
+    var mZanChildClickListener: IOnClickListener<ReplyBean.ChildReplyBean>? = null
+    var mReplyChildClickListener: IOnClickListener<ReplyBean>? = null
+    var mReplyParentClickListener: IOnClickListener<ReplyBean>? = null
+
+
     override fun convert(helper: ReplyViewHold, item: ReplyBean) {
         helper.bindData(mContext, item)
 
+        helper.getView<View>(R.id.tvZan).setOnClickListener {
+            mZanParentListener?.onClick(helper.adapterPosition, item, helper)
 
+        }
 
+        helper.getView<View>(R.id.ivRely).setOnClickListener {
+            mReplyParentClickListener
+        }
     }
 
+
+    interface IOnClickListener<T> {
+        fun onClick(position: Int, t: T, helper: ReplyViewHold)
+    }
 
 }
--- reply/src/main/java/com/example/reply/ReplyFragment.kt
+++ reply/src/main/java/com/example/reply/ReplyFragment.kt
@@ -3,14 +3,16 @@ package com.example.reply
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import cn.dreamtobe.kpswitch.IPanelHeightTarget
+import cn.dreamtobe.kpswitch.util.KeyboardUtil
 import com.gyf.barlibrary.ImmersionBar
 import kotlinx.android.synthetic.main.frg_reply.*
+
 
 class ReplyFragment : Fragment() {
 
@@ -29,26 +31,35 @@ class ReplyFragment : Fragment() {
         rv.adapter = adapter
         initData()
         flBottom.visibility = View.INVISIBLE
-        ImmersionBar.with(activity!!)
-            .navigationBarEnable(false)
-            .navigationBarWithKitkatEnable(false)
-            .navigationBarWithEMUI3Enable(false)
-            .statusBarColorTransformEnable(false)
-            .keyboardEnable(true).setOnKeyboardListener { isPopup, keyboardHeight ->
-            if(!isPopup){
-                KeyboardktUtils.hideKeyboard(etContent)
-                tvBottom.visibility = View.VISIBLE
-                flBottom.visibility = View.INVISIBLE
-                etContent.clearFocus()
+        KeyboardUtil.attach(activity!!,object:IPanelHeightTarget{
+            override fun getHeight(): Int {
+
+                return 200
             }
 
-        }.init()
+            override fun refreshHeight(panelHeight: Int) {
+
+                Log.i("infos","he" + panelHeight)
+            }
+
+            override fun onKeyboardShowing(showing: Boolean) {
+                if(!showing){
+                    KeyboardUtil.hideKeyboard(etContent)
+                    tvBottom.visibility = View.VISIBLE
+                    flBottom.visibility = View.INVISIBLE
+                    etContent.clearFocus()
+                }
+
+            }
+
+        })
+
 
         tvBottom.setOnClickListener {
             tvBottom.visibility = View.INVISIBLE
             flBottom.visibility = View.VISIBLE
             etContent.requestFocus()
-            KeyboardktUtils.showKeyboard(etContent)
+            KeyboardUtil.showKeyboard(etContent)
         }
 
 
@@ -59,11 +70,10 @@ class ReplyFragment : Fragment() {
         for (i in 0..20) {
             list.add(ReplyBean().apply {
                 val arrayListOf = arrayListOf<ReplyBean.ChildReplyBean>()
-                for (j in 1..2){
-
+                for (j in 1..i) {
+                    arrayListOf.add(ReplyBean.ChildReplyBean())
                 }
-                arrayListOf.add(ReplyBean.ChildReplyBean())
-                childReply=arrayListOf
+                childReply = arrayListOf
             })
         }
         adapter.addData(list)
